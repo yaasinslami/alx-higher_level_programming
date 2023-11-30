0d2e929e5a2c3d2c029914c267c71eaa8927b6d2
yaasinslami <yassinx4002@gmail.com>
2023-11-29 16:40:17 -0800

    8 commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
         temp = iterator
         if ord(temp) >= 97 and ord(temp) <= 122:
             temp = chr(ord(iterator) - 32)
-        print("{}".format(temp), end ='')
+        print("{}".format(temp), end='')
 
     print()
