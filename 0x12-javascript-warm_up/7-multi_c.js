e788425f89c5d5e655cb0d1fc001cb96fa7da4aa
yaasinslami <yassinx4002@gmail.com>
2024-06-18 18:38:42 -0700

    0x12-javascript-warm_up project

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,8 @@
 const x = process.argv[2];
 if (isNaN(x)) {
 	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i < x; i++) {
+} else {
+	for (let j = 0; j < x; j++) {
 		console.log('C is fun');
 	}
 }
