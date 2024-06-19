11693ec15103c7b415a8bdabb43872b908f804b5
yaasinslami <yassinx4002@gmail.com>
2024-06-18 19:51:04 -0700

    0x12-javascript-warm_up project

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const variable = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i <= 2; i++) {
-	console.log(variable[i]);
+  console.log(variable[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = process.argv[2];
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let j = 0; j < x; j++) {
-		console.log('C is fun');
-	}
+  for (let j = 0; j < x; j++) {
+    console.log('C is fun');
+  }
 }
