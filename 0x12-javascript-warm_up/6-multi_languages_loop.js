a8babb7cc5cba08c8f4e59fedd6473b72d8006d3
yaasinslami <yassinx4002@gmail.com>
2024-06-18 19:37:20 -0700

    0x12-javascript-warm_up project

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const val = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const phr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let j = 0; j < 3; j++) {
-	console.log(val[j]);
+	console.log(phr[j]);
 }
